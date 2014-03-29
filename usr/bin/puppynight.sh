6592307ac541dd095b4f6adbd7bbe17068b70616
woofshahenzup <copymanuals2@gmail.com>
2014-03-29 00:39:33 -0600

    Traducción de advertencia de bateria baja

--- usr/bin/puppynight.sh
+++ usr/bin/puppynight.sh
@@ -4,7 +4,7 @@
 export TEXTDOMAIN=Poweraplay_tray_puppy night
 export OUTPUT_CHARSET=UTF-8
 
-gtkdialog-splash -fontsize "small" -close box -bg white -timeout 10 -text "'$(gettext 'ADVERTENCIA!!!!  Bateria baja.')'" -icon /usr/share/icons/battery_low.png  &
+gtkdialog-splash -fontsize "small" -close box -bg white -timeout 10 -text "'$(gettext 'WARNING!!!!  Low battery.')'" -icon /usr/share/icons/battery_low.png  &
 
 		
 		
