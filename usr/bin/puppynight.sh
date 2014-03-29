e791996f68ee33c43ec4aead07cfe5ae4737fa88
josejp2424 <puppylinuxjosejp2424@gmail.com>
2014-03-29 03:33:50 -0300

    script de poweraplay_tray

---
+++ usr/bin/puppynight.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#by josejp2424
+#internationalized (nilsonmorales)
+export TEXTDOMAIN=Poweraplay_tray_puppy night
+export OUTPUT_CHARSET=UTF-8
+
+gtkdialog-splash -fontsize "small" -close box -bg white -timeout 10 -text "'$(gettext 'ADVERTENCIA!!!!  Bateria baja.')'" -icon /usr/share/icons/battery_low.png  &
+
+		
+		
